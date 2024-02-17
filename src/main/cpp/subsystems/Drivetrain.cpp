2dbf14614981b4232f4233f6d6978443dfbded14
Galileo Robotics <71620129+Alvin-He@users.noreply.github.com>
2024-02-16 16:56:55 -0800

    Implemented odometry
    Authored By: Nicolas Mattson

--- src/main/cpp/subsystems/Drivetrain.cpp
+++ src/main/cpp/subsystems/Drivetrain.cpp
@@ -6,6 +6,8 @@
 #include "frc4669.h"
 #include <ctre/phoenix6/controls/Follower.hpp>
 #include <units/voltage.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <units/time.h>
 
 Drivetrain::Drivetrain() {
     frc4669::ConfigureMotor(leftMainMotor, true);
@@ -15,12 +17,20 @@ Drivetrain::Drivetrain() {
     frc4669::ConfigureMotor(rightMainMotor, false);
     frc4669::ConfigureMotor(rightSecondaryMotor, false);
     rightSecondaryMotor.SetControl(rightFollower);
-
 }
 
 // This method will be called once per scheduler run
 
-void Drivetrain::Periodic() {}
+void Drivetrain::Periodic() {
+    m_odometry.Update(frc::Rotation2d(GetYaw()), GetLeftDistance(), GetRightDistance());
+
+    frc::Pose2d robotPose = m_odometry.GetPose();
+
+    m_field->SetRobotPose(robotPose);
+
+    frc::SmartDashboard::PutNumber("Yaw", GetYaw().value());
+    frc::SmartDashboard::PutNumber("Pitch", GetPitch().value());
+}
 
 void Drivetrain::CurvatureDrive(double forward, double rotation){
     drive.CurvatureDrive(forward, rotation, motorTurnInPlace);
@@ -38,4 +48,48 @@ frc2::CommandPtr Drivetrain::DefaultDriveCommand(std::function<double()> speed, 
     return Run([this, speed = std::move(speed), rotation = std::move(rotation)]{
         drive.CurvatureDrive(speed(), rotation(), true);
     });
+}
+
+frc::DifferentialDriveWheelSpeeds Drivetrain::GetWheelSpeeds() {
+  units::meters_per_second_t leftVelocity = units::meters_per_second_t(
+    leftMainMotor.GetRotorVelocity().GetValue() * 10 * DriveConstants::kMetersPerTick
+  );
+
+  units::meters_per_second_t rightVelocity = units::meters_per_second_t(
+    -(rightMainMotor.GetRotorVelocity().GetValue() * 10 * DriveConstants::kMetersPerTick)
+  );
+
+  return { leftVelocity, rightVelocity };
+}
+
+units::meter_t Drivetrain::GetLeftDistance() {
+  return units::meter_t(leftMainMotor.GetPosition().GetValue() * DriveConstants::kMetersPerTick);
+}
+
+units::meter_t Drivetrain::GetRightDistance() {
+  return units::meter_t(-(rightMainMotor.GetPosition().GetValue() * DriveConstants::kMetersPerTick));
+}
+
+units::degree_t Drivetrain::GetYaw() {
+  return units::degree_t(-m_IMU.GetYaw()) - m_yawOffset;
+}
+
+units::degree_t Drivetrain::GetPitch() {
+  return units::degree_t(m_IMU.GetPitch());
+}
+
+frc::Pose2d Drivetrain::OdometryPose() {
+  return m_odometry.GetPose();
+}
+
+void Drivetrain::ResetEncoders() {
+  leftMainMotor.SetPosition(units::angle::turn_t(0));
+  rightMainMotor.SetPosition(units::angle::turn_t(0));
+}
+
+void Drivetrain::ResetOdometry(frc::Pose2d pose, frc::Rotation2d rotation) {
+  ResetEncoders();
+  m_IMU.ZeroYaw();
+  m_yawOffset = rotation.Degrees();
+  m_odometry.ResetPosition(rotation, 0_m, 0_m, pose);
 }
--- src/main/include/subsystems/Drivetrain.h
+++ src/main/include/subsystems/Drivetrain.h
@@ -14,6 +14,14 @@
 #include <frc/drive/DifferentialDrive.h>
 #include <frc2/command/CommandPtr.h>
 #include <ctre/phoenix6/controls/Follower.hpp>
+#include <frc/kinematics/DifferentialDriveOdometry.h>
+#include <frc/geometry/Rotation2d.h>
+#include <frc/geometry/Pose2d.h>
+#include <frc/smartdashboard/Field2d.h>
+#include <units/length.h>
+#include <units/angle.h>
+#include <AHRS.h>
+#include <ctre/phoenix6/CANcoder.hpp>
 
 //setup 4 motors for drive 
 // 
@@ -32,6 +40,13 @@ class Drivetrain : public frc2::SubsystemBase {
 
   frc2::CommandPtr DefaultDriveCommand(std::function<double()> speed, std::function<double()> rotation);
 
+  frc::DifferentialDriveWheelSpeeds GetWheelSpeeds();
+  units::meter_t GetLeftDistance();
+  units::meter_t GetRightDistance();
+  units::degree_t GetYaw();
+  units::degree_t GetPitch();
+  frc::Pose2d OdometryPose();
+
  private:
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
@@ -44,6 +59,15 @@ class Drivetrain : public frc2::SubsystemBase {
   ctre::phoenix6::controls::Follower rightFollower { CAN::kRightMainMotor, false};
 
   frc::DifferentialDrive drive { leftMainMotor, rightMainMotor };
+
+  AHRS m_IMU { frc::SPI::Port::kMXP };
+  units::degree_t m_yawOffset = 0_deg;
+
+  frc::DifferentialDriveOdometry m_odometry { frc::Rotation2d(), 0.0_m, 0.0_m, frc::Pose2d() };
+  frc::Field2d* m_field;
+
+  void ResetOdometry(frc::Pose2d pose, frc::Rotation2d rotation);
+  void ResetEncoders();
 
   bool motorTurnInPlace = true;
 };
